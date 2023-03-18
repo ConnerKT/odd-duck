1ec942b48f6675251b16818b5b19ddd57c23cff2
ConnerKT <connerthompson121@gmail.com>
2023-03-18 01:43:20 -0500

    JS Addition

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// Creating constructor function for products
+// This declares 3 properties, the name of the prod., its file path, and how many times its shown
+
+function productDisplay(name,path,shown){
+
+    this.name = name;
+    this.name = path;
+    this.shown = shown;
+    this.variable = function(){
+        
+    }
+}
