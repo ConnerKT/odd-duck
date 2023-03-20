f07ccaa9680f1f9a94124224505a96d0e3e3081c
ConnerKT <connerthompson121@gmail.com>
2023-03-19 22:20:44 -0500

    Img Update
    IMGs now appear

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let container = document.querySelector("section");
 let product1 = document.querySelector('section img:first-child');
 let product2 = document.querySelector('section img:nth-child(2)');
 let product3 = document.querySelector('section img:nth-child(3)');
-let button = document.querySelector("button")
+let button = document.querySelector('button');
 
 // assign a global variable for clicks, so when the program is run the variable is set to 0
 
@@ -19,14 +19,14 @@ let totalClicks = 25;
 
 let state = {
   duckArray : [],
-}
+};
 
 // Forming constructor function to OOP code
 
-function products(name,path,views){
+function Products(name,path,views){
 
   this.name = name;
-  this.name = path;
+  this.path = path;
   this.views = views;
   this.clicks = 0;
 }
@@ -45,46 +45,55 @@ function render(){
   let value1 = getRandom();
   let value2 = getRandom();
   let value3 = getRandom();
+  
 
   //The products are the img tags in HTML, we are targetting the src and alt of each img tag to randomzie the img shown on each
+  while (value1 === value2){
+    value2 = getRandom();
+  }
 
-  product1.src = state.duckArray[value1].src
-  product2.src = state.duckArray[value2].src
-  product3.src = state.duckArray[value3].src
-  product1.alt = state.duckArray[value1].name
-  product1.alt = state.duckArray[value2].name
-  product1.alt = state.duckArray[value3].name
+  product1.src = state.duckArray[value1].path;
+  product2.src = state.duckArray[value2].path;
+  product3.src = state.duckArray[value3].path;
+  product1.alt = state.duckArray[value1].name;
+  product2.alt = state.duckArray[value2].name;
+  product3.alt = state.duckArray[value3].name;
 
   // Setting the views for each randomized img to increment up once since it was viewed
 
-  state.duckArray[value1] = views++;
-  state.duckArray[value2] = views++;
-  state.duckArray[value3] = views++;
+  state.duckArray[value1].views++;
+  state.duckArray[value2].views++;
+  state.duckArray[value3].views++;
+}
+function duckDisplay(event){
+
 }
 
 // Assigning New objects through the constructor function
 
-let bag = products('R2D2 Bag', './img/bag.jpg', 0);
-let banana = products('Banana Cutter', './img/banana.jpg', 0);
-let bathroom = products('Toilet Paper Stand', './img/bathroom.jpg', 0);
-let boots = products('Rain Boots', './img/boots.jpg', 0);
-let breakfast = products('Breakfast Appliance', './img/breakfast.jpg', 0);
-let bubblegum = products('Meatball Gum', './img/bubblegum.jpg', 0);
-let chair = products('Red Chair', './img/chair.jpg', 0);
-let cthulhu = products('Cthulhu Figurine', './img/cthulhu.jpg', 0);
-let dogduck = products('Dog Duck Mask', './img/dog-duck.jpg', 0);
-let dragon = products('Dragon Meat', './img/dragon.jpg', 0);
-let pen = products('Utensil Pens', './img/pen.jpg', 0);
-let petsweep = products('Pet Feet Sweeper', './img/pet-sweep.jpg', 0);
-let scissors = products('Pizza Scissors', './img/scissors.jpg', 0);
-let shark = products('Shark Sleeping Bag', './img/shark.jpg', 0);
-let sweep = products('Baby Sweeper', './img/sweep.jpg', 0);
-let tauntaun = products('Tauntaun Sleeping Bag', './img/tauntaun.jpg', 0);
-let unicorn = products('Unicorn Meat', './img/unicorn.jpg', 0);
-let watercan = products('Watering Can', './img/watercan.jpg', 0);
-let wineglass = products('Fancy Wine Glass', './img/wineglass.jpg', 0);
+let bag = new Products('R2D2 Bag', './img/bag.jpg', 0);
+let banana = new Products('Banana Cutter', './img/banana.jpg', 0);
+let bathroom = new Products('Toilet Paper Stand', './img/bathroom.jpg', 0);
+let boots = new Products('Rain Boots', './img/boots.jpg', 0);
+let breakfast = new Products('Breakfast Appliance', './img/breakfast.jpg', 0);
+let bubblegum = new Products('Meatball Gum', './img/bubblegum.jpg', 0);
+let chair = new Products('Red Chair', './img/chair.jpg', 0);
+let cthulhu = new Products('Cthulhu Figurine', './img/cthulhu.jpg', 0);
+let dogduck = new Products('Dog Duck Mask', './img/dog-duck.jpg', 0);
+let dragon = new Products('Dragon Meat', './img/dragon.jpg', 0);
+let pen = new Products('Utensil Pens', './img/pen.jpg', 0);
+let petsweep = new Products('Pet Feet Sweeper', './img/pet-sweep.jpg', 0);
+let scissors = new Products('Pizza Scissors', './img/scissors.jpg', 0);
+let shark = new Products('Shark Sleeping Bag', './img/shark.jpg', 0);
+let sweep = new Products('Baby Sweeper', './img/sweep.png', 0);
+let tauntaun = new Products('Tauntaun Sleeping Bag', './img/tauntaun.jpg', 0);
+let unicorn = new Products('Unicorn Meat', './img/unicorn.jpg', 0);
+let watercan = new Products('Watering Can', './img/water-can.jpg', 0);
+let wineglass = new Products('Fancy Wine Glass', './img/wine-glass.jpg', 0);
 
+
+
+state.duckArray.push(bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogduck, dragon, pen, petsweep, scissors, shark, sweep, tauntaun, unicorn, watercan, wineglass);
 
 render();
-state.duckArray.push(bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu. dogduck. dragon, pen, petsweep, scissors, shark, sweep, tauntaun, unicorn, watercan, wineglass);
 
